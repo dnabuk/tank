c73c0213c67d15e4d5a1d7ef90ebec4826ce75f4
Piotr <pittero1@wp.pl>
2018-07-06 12:48:25 +0200

    dzien 5 - startujemy projekt:) v5

--- MainGame.php
+++ MainGame.php
@@ -15,6 +15,117 @@ class MainGame
 		return $result;
 	}
 	
+	public function GetBoard()
+	{
+		$ch = curl_init(GAMEURL.'/get-current-board.php');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+		$result = curl_exec($ch);
+		$result = json_decode($result);
+		curl_close($ch);
+		return $result;
+	}
+	public function CheckPositionBoard($id, $data)
+	{
+		$board = $data->board;
+		for($i=0; $i<count($board); $i++)
+		{
+			if($board{$i}->position == $id)
+			{
+				$type = $board{$i}->type;
+				if($type == 'empty')
+				{
+					return 'white';
+				}
+				else if($type == 'obstacle')
+				{
+					return 'red';
+				}
+				else if($type == 'player')
+				{
+					return 'green';
+				}
+				else if($type == 'player_base')
+				{
+					return 'blue';
+				}
+				else if($type == 'missile')
+				{
+					return 'black';
+				}				
+				return false;
+			}
+		}
+		return false;
+	}
+	public function DrawBoard($data)
+	{
+		$boardSize = $data->settings->boardSize;
+		$boardSizeLetters = $this->getNameFromNumber($boardSize);
+		
+		echo '<table>';
+		for($j = 1; $j <=$boardSize; $j++)
+		{
+			echo '<tr>';
+			for ($i = 'A'; $i !== ''.$boardSizeLetters.''; $i++)
+			{
+				$color = $this->CheckPositionBoard($i.$j, $data);
+				if($color)
+				{
+					echo '<td bgcolor="'.$color.'">';
+					echo $i.$j;
+					echo '</td>';					
+				}
+				else
+				{
+					echo '<td>';
+					echo $i.$j;
+					echo '</td>';					
+				}
+			}
+			echo '</tr>';
+		}
+		echo '</table>';
+		
+	}
+	public function getNameFromNumber($num)
+	{
+		$numeric = $num % 26;
+		$letter = chr(65 + $numeric);
+		$num2 = intval($num / 26);
+		if ($num2 > 0) {
+			return $this->getNameFromNumber($num2 - 1) . $letter;
+		} else {
+			return $letter;
+		}
+	}
+	public function sortObjectsByByField($objects, $fieldName, $sortOrder = SORT_ASC, $sortFlag = SORT_REGULAR)
+	{
+		$sortFields = array();
+		foreach ($objects as $key => $row) {
+			$sortFields[$key] = $row->{$fieldName};
+		}
+		array_multisort($sortFields, $sortOrder, $sortFlag, $objects);
+		return $objects;
+	}
+	public function CheckMyPlayerName()
+	{
+		$ch = curl_init(GAMEURL.'/get-current-board.php');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+		$result = curl_exec($ch);
+		$result = json_decode($result);
+		curl_close($ch);
+
+		for($i=0; $i<count($result->players); $i++)
+		{
+			if($result->players{$i}->name == PLAYERNAME)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 	public function MoveTo($key, $direction, $distance, $fire)
 	{
 		$data = array(
@@ -23,6 +134,7 @@ class MainGame
 				'distance' => $distance,
 				'fire' => $fire,
 		);
+		
 		$ch = curl_init(GAMEURL.'/make-move.php');
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_POST, true);
--- index.php
+++ index.php
@@ -1,19 +1,55 @@
 <?php
-//02dab45610fa718dcf1d06fb514abb8d 
+session_start();
+
 define("GAMEURL", "http://tank.iai.ninja/api");
 define("PLAYERNAME", "Team_5");
+define("KEY", "02dab45610fa718dcf1d06fb514abb8d");
+
 
 require_once('MainGame.php');
+$Main = new MainGame();
 
-//Logowanie do gry start
-$Login = new MainGame();
-$LoginResult = $Login->Login();
+//Sekcja odpowiedzialna za logowanie START
+if(!$Main->CheckMyPlayerName())
+{
+	$LoginResult = $Main->Login();
+	if($LoginResult->errno == 0)
+	{
+		$_SESSION['key'] = $LoginResult->data->key;
+	}
+	else 
+	{
+		echo 'Error Login no. '.$LoginResult->data->key;
+	}
+}
+//Sekcja odpowiedzialna za logowanie END
 
-if($LoginResult->errno == 0)
-{
-	print_r($LoginResult);
-}
-else 
-{
-	echo 'Error no: '.$LoginResult->errno;
-}
+//Pobieranie pozycji i decyzja o ruchu START
+$GetBoardResult = $Main->GetBoard();
+$DrawBoardResult = $Main->DrawBoard($GetBoardResult);
+
+print_r($DrawBoardResult);
+
+
+
+
+
+
+
+//Pobieranie pozycji i decyzja o ruchu END
+die;
+
+
+
+
+
+
+//Wykonywanie ruchow START
+$direction = 'S';
+$distance = '1';
+$fire = false;
+
+$MoveResult = $Main->MoveTo($_SESSION['key'], $direction, $distance, $fire);
+
+print_r($_SESSION['key']);
+//Wykonywanie ruchow END
