18695a27df08bcc1ac52549869c40ccc47ef6ccc
Piotr <pittero1@wp.pl>
2018-07-06 09:57:20 +0200

    dzien 5 - startujemy projekt:) v1

--- Autoryzacja.php
+++ Autoryzacja.php
@@ -1,6 +1,9 @@
 <?php
 class LoginToGame
 {
+	private static $name = 'Team_5';
+	private static $url = 'http://tank.iai.ninja/api/';
+	
 	public function __construct()
 	{
 		
@@ -8,16 +11,15 @@ class LoginToGame
 	
 	public function Login()
 	{
-		$data = array('name' => 'Team_5');
-		$ch = curl_init('http://tank.iai.ninja/api/join-current-game.php');
+		$data = array('name' => self::$name);
+		$ch = curl_init(self::$url.'join-current-game.php');
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_POST, true);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 		$result = curl_exec($ch);
-		print_r($result);
 		$result = json_decode($result);
 		curl_close($ch);
+		return $result;
 	}
 }
-$login = new LoginToGame();
-print_r($login->Login());
